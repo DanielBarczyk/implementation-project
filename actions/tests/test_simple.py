43462684b3eee6415ceac5b0ab3e85fca6735d4f
Daniel Barczyk <d.barczyk99@gmail.com>
2025-03-25 22:13:37 +0100

    feat: sample Python project for Github Actions

---
+++ actions/tests/test_simple.py
@@ -0,0 +1,12 @@
+import unittest
+
+from sample.simple import increment
+
+class TestSimple(unittest.TestCase):
+
+    def test_add_one(self):
+        self.assertEqual(increment(5), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
