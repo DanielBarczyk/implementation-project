1f4465858cb006bf1cb6fc37a84374a27cd44b70
DanielBarczyk <d.barczyk99@gmail.com>
2025-03-27 17:23:57 +0100

    feat: live wrong

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -8,8 +8,8 @@ class TestSimple(unittest.TestCase):
     def test_add_one(self):
         self.assertEqual(increment(5), 6)
 
-    def test_add_one(self):
-        self.assertEqual(increment(5), 7)
+    def test_add_one_2(self):
+        self.assertEqual(increment(5), 6)
 
 
 if __name__ == '__main__':
